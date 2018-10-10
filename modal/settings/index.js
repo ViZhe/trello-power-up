57e1423ef15926db575d91b932a7b1c952393639
ViZhe <vizhe.cat@ya.ru>
2018-10-10 16:19:03 +0300

    Add output data

--- modal/settings/index.js
+++ modal/settings/index.js
@@ -2,18 +2,26 @@
 
 const t = TrelloPowerUp.iframe()
 
-
 t.render(() => {
   console.log('ready render')
+  t.getAll()
+    .then((data) => {
+      console.log(JSON.stringify(data, null, 2))
+    })
+
+  const projectSelector = document.getElementById('projectList')
+  t.get('organization', 'shared', 'PROJECTS')
+    .then((projectList) => {
+      projectList.each((item) => {
+        projectSelector.insertAdjacentHTML('afterend', `<option value="${item}">${item}</option>`)
+      })
+    })
 })
 
 
 document.getElementById('projectInputSave').addEventListener('click', () => {
   const projectInput = document.getElementById('projectInput')
   t.set('organization', 'shared', 'PROJECTS', projectInput.value)
-    .then(() => {
-      t.closePopup()
-    })
 })
 
 document.addEventListener('click', (e) => {
